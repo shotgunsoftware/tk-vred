ae7bf26fabeeed1afd9319aa3dd9796846ff531d
Stacey Oue <stacey.oue@autodesk.com>
2020-07-14 13:15:56 -0400

    [SHOT-3445] VRED Plugin UI Box
    * Style Shotgun plugin dialog since it does not seem the menu option can be exlcuded using the VRED Python API
    * Add animation gif file to the plugin folder for the dialog to display
    * Remove unused vrShotgun method 'getVredMainWindow' (and the engine defines a more robust method to get the window)

--- plugins/Shotgun/vrShotgun.py
+++ plugins/Shotgun/vrShotgun.py
@@ -1,6 +1,6 @@
 import os
 
-from PySide2 import QtCore
+from PySide2 import QtCore, QtGui
 from PySide2 import QtWidgets
 
 import vrFileIO
@@ -17,11 +17,20 @@ vrShotgun_form, vrShotgun_base = uiTools.loadUiType("vrShotgunGUI.ui")
 class vrShotgun(vrShotgun_form, vrShotgun_base):
     context = None
     engine = None
+    infoGif = None
 
     def __init__(self, parent=None):
         super(vrShotgun, self).__init__(parent)
         parent.layout().addWidget(self)
         self.setupUi(self)
+
+        # Set up the gif animation, but don't start playing it until the widget is shown
+        gif_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "vred_shotgun_menu.gif"
+        )
+        self.infoGif = QtGui.QMovie(gif_path)
+        self._gifLabel.setMovie(self.infoGif)
+
         self.context = sgtk.context.deserialize(os.environ.get("SGTK_CONTEXT"))
         QtCore.QTimer.singleShot(0, self.init)
 
@@ -29,6 +38,10 @@ class vrShotgun(vrShotgun_form, vrShotgun_base):
         self.engine = sgtk.platform.start_engine(
             "tk-vred", self.context.sgtk, self.context
         )
+
+        # Set the version text in the plugin dialog once the engine is initialized
+        self._versionLabel.setText("tk-vred {}".format(self.engine.version))
+
         file_to_open = os.environ.get("SGTK_FILE_TO_OPEN", None)
         if file_to_open:
             vrController.newScene()
@@ -37,10 +50,23 @@ class vrShotgun(vrShotgun_form, vrShotgun_base):
     def __del__(self):
         self.destroyMenu()
 
-    def getVredMainWindow(self):
-        from shiboken2 import wrapInstance
+    def showEvent(self, event):
+        """
+        Reimplement QWidget event handler to receive signal when this widget
+        becomes visible, at which point the gif will start playing.
 
-        return wrapInstance(VREDMainWindowId, QtWidgets.QMainWindow)
+        :param QShowEvent event: event that is sent when the widget is shown.
+        """
+        self.infoGif.start()
+
+    def hideEvent(self, event):
+        """
+        Reimplement QWidget event handler to receive signal when this widget
+        is hidden, at which point the gif will stop playing.
+
+        :param QHideEvent event: event that is sent when the widget is hidden.
+        """
+        self.infoGif.stop()
 
 
 try:
