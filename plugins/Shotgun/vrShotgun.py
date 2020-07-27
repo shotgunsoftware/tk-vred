3f55e5fa498c60e634e780e2b920860c57eebf8d
Stacey Oue <stacey.oue@autodesk.com>
2020-07-27 14:33:33 -0400

    [SHOT-3445] VRED Plugin UI Box (#33)
    [SHOT-3445] VRED Plugin UI Box
    
    * Style Shotgun plugin dialog since it cannot be excluded using the VRED Python API
    * Add animation gif file to the plugin folder for the dialog to display
    * Remove unused vrShotgun method 'getVredMainWindow' (and the engine defines a more robust method to get the window)

--- plugins/Shotgun/vrShotgun.py
+++ plugins/Shotgun/vrShotgun.py
@@ -1,7 +1,9 @@
+# For Python2 integer division
+from __future__ import division
+
 import os
 
-from PySide2 import QtCore
-from PySide2 import QtWidgets
+from PySide2 import QtCore, QtGui
 
 import vrFileIO
 import vrController
@@ -17,11 +19,20 @@ vrShotgun_form, vrShotgun_base = uiTools.loadUiType("vrShotgunGUI.ui")
 class vrShotgun(vrShotgun_form, vrShotgun_base):
     context = None
     engine = None
+    gif_aspect_ratio = None
 
     def __init__(self, parent=None):
         super(vrShotgun, self).__init__(parent)
         parent.layout().addWidget(self)
         self.setupUi(self)
+
+        # Set up the gif animation, but don't start playing it until the widget is shown
+        gif_movie = QtGui.QMovie("vred_shotgun_menu.gif")
+        gif_movie.jumpToFrame(0)
+        movie_size = gif_movie.currentImage().size()
+        self.gif_aspect_ratio = movie_size.width() / movie_size.height()
+        self.gif_label.setMovie(gif_movie)
+
         self.context = sgtk.context.deserialize(os.environ.get("SGTK_CONTEXT"))
         QtCore.QTimer.singleShot(0, self.init)
 
@@ -29,6 +40,10 @@ class vrShotgun(vrShotgun_form, vrShotgun_base):
         self.engine = sgtk.platform.start_engine(
             "tk-vred", self.context.sgtk, self.context
         )
+
+        # Set the version text in the plugin dialog once the engine is initialized
+        self.version_label.setText("tk-vred {}".format(self.engine.version))
+
         file_to_open = os.environ.get("SGTK_FILE_TO_OPEN", None)
         if file_to_open:
             vrController.newScene()
@@ -37,10 +52,47 @@ class vrShotgun(vrShotgun_form, vrShotgun_base):
     def __del__(self):
         self.destroyMenu()
 
-    def getVredMainWindow(self):
-        from shiboken2 import wrapInstance
+    def showEvent(self, event):
+        """
+        Reimplement QWidget event handler to receive signal when this widget
+        becomes visible, at which point the gif will start playing.
 
-        return wrapInstance(VREDMainWindowId, QtWidgets.QMainWindow)
+        :param QShowEvent event: event that is sent when the widget is shown.
+        """
+        gif_movie = self.gif_label.movie()
+        if gif_movie:
+            gif_movie.start()
+
+    def hideEvent(self, event):
+        """
+        Reimplement QWidget event handler to receive signal when this widget
+        is hidden, at which point the gif will stop playing.
+
+        :param QHideEvent event: event that is sent when the widget is hidden.
+        """
+        gif_movie = self.gif_label.movie()
+        if gif_movie:
+            gif_movie.stop()
+
+    def resizeEvent(self, event):
+        """Reimplement resize event handler to keep gif animation aspect ratio."""
+        rect = self.geometry()
+        gif_movie = self.gif_label.movie()
+        if gif_movie:
+            width = rect.height() * self.gif_aspect_ratio
+            if width <= rect.width():
+                size = QtCore.QSize(width, rect.height())
+            else:
+                height = rect.width() / self.gif_aspect_ratio
+                size = QtCore.QSize(rect.width(), height)
+
+            # Scale the gif animation and then adjust this widget and the most
+            # top-level widget to fit to the new gif scaled size
+            gif_movie.setScaledSize(size)
+            self.adjustSize()
+            self.parentWidget().parentWidget().adjustSize()
+
+        return super(vrShotgun, self).resizeEvent(event)
 
 
 try:
