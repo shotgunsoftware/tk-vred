45ca855e86c03b0bb348d1f56fc2636ae0288b97
Stacey Oue <stacey.oue@autodesk.com>
2022-09-22 16:10:12 -0400

    Ensure the parent index is also valid to check if it is a top group node.

--- hooks/tk-multi-data-validation/basic/ui_config.py
+++ hooks/tk-multi-data-validation/basic/ui_config.py
@@ -27,7 +27,9 @@ class VREDUIConfig(HookBaseClass):
 
         # The default is the palette's midlight color. Use the alternate base color for more
         # contrast between rule item rows and the background.
-        if index.parent():
+        parent_index = index.parent()
+        print(parent_index, parent_index.isValid())
+        if parent_index and parent_index.isValid():
             return QtGui.QApplication.palette().alternateBase()
 
         return QtGui.QApplication.palette().midlight()
