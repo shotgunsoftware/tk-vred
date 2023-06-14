9ee33310fea96c2b57c36309bb090d85fb6197d6
unknown <stacey.oue@autodesk.com>
2023-06-14 16:30:00 -0400

    Fix VRED process hanging on quit and reload crash.
    * Use the tank dialog widget, instead of the dialog's widget instance to
      set as the QDockWidget's main widget
    * This ensures that the tank dialog close event is triggered, which then
      triggers the widget instance's close event, to allow VRED to exit
      properly

--- engine.py
+++ engine.py
@@ -619,16 +619,16 @@ class VREDEngine(sgtk.platform.Engine):
         # traversing all the application widgets may point to stale widgets and
         # attempting to reuse them may cause errors (e.g. there have been issues
         # with reusing Shotgun Panel App by finding it from QApplication.allWidgets())
-        widget_instance = None
+        dialog_widget = None
         for dock_widget_panel_id, dock_widget in self._dock_widgets.items():
             if dock_widget_panel_id == panel_id:
-                widget_instance = dock_widget.widget()
+                dialog_widget = dock_widget.widget()
                 parent = self._get_dialog_parent()
-                widget_instance.setParent(parent)
+                dialog_widget.setParent(parent)
                 break
 
-        if not widget_instance:
-            _, widget_instance = self._create_dialog_with_widget(
+        if not dialog_widget:
+            dialog_widget, widget = self._create_dialog_with_widget(
                 title, bundle, widget_class, *args, **kwargs
             )
 
@@ -636,14 +636,14 @@ class VREDEngine(sgtk.platform.Engine):
         dock_area = dock_properties.get("pos", QtCore.Qt.RightDockWidgetArea)
         tabbed = dock_properties.get("tabbed", True)
         self.show_dock_widget(
-            panel_id, title, widget_instance, dock_area=dock_area, tabbed=tabbed
+            panel_id, title, dialog_widget, dock_area=dock_area, tabbed=tabbed
         )
 
         # Return the widget created by the method, _create_dialog_with_widget, since this will
         # have the widget_class type expected by the caller. This widget represents the panel
         # so it should have the object name set to the panel_id
-        widget_instance.setObjectName(panel_id)
-        return widget_instance
+        dialog_widget.setObjectName(panel_id)
+        return dialog_widget
 
     def show_dock_widget(self, panel_id, title, widget, dock_area=None, tabbed=False):
         """
